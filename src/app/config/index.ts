5a62b34a75e2c4fe5dc20f2880b25c17df40e424
Rajib Sarker <redhat0593@gmail.com>
2024-05-21 00:05:58 +0600

    mdb connected

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -2,4 +2,5 @@ require("dotenv").config();
 
 export default {
   port: process.env.PORT,
+  db_uri: process.env.MURI,
 };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
 import app from "./app";
 import config from "./app/config";
+import mongoose from "mongoose";
 
-app.listen(config.port, () => {
-  console.log(`My app listening on port ${config.port}`);
-});
+async function main() {
+  await mongoose.connect(config.db_uri as string);
+
+  app.listen(config.port, () => {
+    console.log(`My app listening on port ${config.port}`);
+  });
+}
+
+main();
